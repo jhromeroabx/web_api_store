3dc26e19d5ceee01f30cdf3b952612d5623090cd
JHOSEP ADBEL ROMERO LOA <jhromero.abx@gmail.com>
2022-04-13 00:27:52 -0500

    CRUD user SP && response

--- src/routes/users.js
+++ src/routes/users.js
@@ -85,4 +85,100 @@ try {
   console.error("ERROR AT: /login", error);
 }
 
+try {
+  router.post("/AddUserOrEdit", (req, res) => {
+    const estado = 1; //siempre activo
+    const idUserType = 5; //siempre user default
+
+    const {
+      id,
+      nombre,
+      apellido,
+      dni,
+      telefono,
+      email,
+      fechaNacimiento,
+      contrasenia,
+    } = req.body;
+
+    if (
+      String(id).length == 0 ||
+      String(nombre).length == 0 ||
+      String(apellido).length == 0 ||
+      String(dni).length == 0 ||
+      String(telefono).length == 0 ||
+      String(email).length == 0 ||
+      String(fechaNacimiento).length == 0 ||
+      String(contrasenia).length == 0
+    ) {
+      res.status(200).send({
+        response: "Algunos campos o todos estan vacios!!!",
+        state: false,
+      });
+    } else if (String(nombre).length > 50) {
+      res.status(200).send({
+        response: "el nombre no puede ser mayor de 50 caracteres!",
+        state: false,
+      });
+    } else if (String(apellido).length > 50) {
+      res.status(200).send({
+        response: "el apellido no puede ser mayor a 50 caracteres!",
+        state: false,
+      });
+    } else if (String(dni).length > 8 || String(dni).length < 8) {
+      res.status(200).send({
+        response: "la dni tiene 8 caracteres!",
+        state: false,
+      });
+    } else if (String(telefono).length > 15) {
+      res.status(200).send({
+        response: "el telefono no puede ser mayor a 15 caracteres!",
+        state: false,
+      });
+    } else if (String(email).length > 50) {
+      res.status(200).send({
+        response: "el correo no puede ser mayor a 50 caracteres!",
+        state: false,
+      });
+    } else if (String(contrasenia).length > 15) {
+      res.status(200).send({
+        response: "la contrasenia no puede ser mayor a 15 caracteres!",
+        state: false,
+      });
+    } else {
+      const query = "CALL userAddOrEdit(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+      mysqlConnection.query(
+        query,
+        [
+          id,
+          nombre,
+          apellido,
+          dni,
+          telefono,
+          email,
+          fechaNacimiento,
+          estado,
+          idUserType,
+          contrasenia,
+        ],
+        (err, rows, fields) => {
+          if (err) {
+            res.json("ERROR AT: /AddUserOrEdit", err);
+          } else {
+            const [RowDataPacket] = rows[0];
+            const { state } = RowDataPacket;
+            if (state == 1) {
+              res.json({ response: RowDataPacket, state: true });  
+            } else {
+              res.json({ response: RowDataPacket, state: false });
+            }
+          }
+        }
+      );
+    }
+  });
+} catch (error) {
+  console.error("ERROR AT: /AddUserOrEdit", error);
+}
+
 module.exports = router;
