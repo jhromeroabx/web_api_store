e637ce6bead5a99b52a3b8967aef1f1fe82cf38f
Jhosep Adbel Romero Loa <jhromero.abx@gmail.com>
2022-07-05 02:17:05 -0500

    fix sonar pt1

--- src/routes/ingreso_salida.js
+++ src/routes/ingreso_salida.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const connectMysql = require("../database");
 
-router.get("/ingresos&salidas", (req, res) => {
+router.get("/ingresos&salidas", (_req, res) => {
   res.json(
     "HOLA ACA SE GESTIONARA TODOS LOS INGRESOS Y SALIDAS DE PRODUCTOS PARA ALMACEN!!!"
   );
@@ -14,14 +14,12 @@ router.get("/getAllIngresos", (req, res) => {
   try {
     let mysqlConnection = connectMysql("/getAllIngresos");
 
-    const {
-      id_user_responsable,
-    } = req.body;
+    const { id_user_responsable } = req.body;
 
     mysqlConnection.query(
       "CALL getAllIngresos(?);",
       [id_user_responsable],
-      (err, rows, fields) => {
+      (err, rows, _fields) => {
         if (err) {
           res
             .status(500)
@@ -29,11 +27,15 @@ router.get("/getAllIngresos", (req, res) => {
           console.error("ERROR AT: /getAllIngresos: ", err);
         } else {
           let rptaFromMysql;
-          [rptaFromMysql] = rows[0];// si encerramos entre corchetes obtenemos el objeto del arreglo
+          [rptaFromMysql] = rows[0]; // si encerramos entre corchetes obtenemos el objeto del arreglo
           const { state, response } = rptaFromMysql;
           if (state === 1) {
             rptaFromMysql = rows[1];
-            res.json({ state: state, response: response, content: rptaFromMysql });
+            res.json({
+              state: state,
+              response: response,
+              content: rptaFromMysql,
+            });
           } else {
             res.json({ state: state, response: response, content: null });
           }
@@ -51,14 +53,12 @@ router.get("/getAllRetiros", (req, res) => {
   try {
     let mysqlConnection = connectMysql("/getAllRetiros");
 
-    const {
-      id_user_responsable,
-    } = req.body;
+    const { id_user_responsable } = req.body;
 
     mysqlConnection.query(
       "CALL getAllRetiros(?);",
       [id_user_responsable],
-      (err, rows, fields) => {
+      (err, rows, _fields) => {
         if (err) {
           res
             .status(500)
@@ -66,11 +66,15 @@ router.get("/getAllRetiros", (req, res) => {
           console.error("ERROR AT: /getAllRetiros: ", err);
         } else {
           let rptaFromMysql;
-          [rptaFromMysql] = rows[0];// si encerramos entre corchetes obtenemos el objeto del arreglo
+          [rptaFromMysql] = rows[0]; // si encerramos entre corchetes obtenemos el objeto del arreglo
           const { state, response } = rptaFromMysql;
           if (state === 1) {
             rptaFromMysql = rows[1];
-            res.json({ state: state, response: response, content: rptaFromMysql });
+            res.json({
+              state: state,
+              response: response,
+              content: rptaFromMysql,
+            });
           } else {
             res.json({ state: state, response: response, content: null });
           }
@@ -94,7 +98,7 @@ router.post("/getAllIngresosDetailById", (req, res) => {
     mysqlConnection.query(
       "CALL getAllIngresosDetailById(?, ?);",
       [id_ingreso, id_user_responsable],
-      (err, rows, fields) => {
+      (err, rows, _fields) => {
         if (err) {
           res
             .status(500)
@@ -109,25 +113,23 @@ router.post("/getAllIngresosDetailById", (req, res) => {
     mysqlConnection.end();
   } catch (error) {
     console.error("ERROR AT: /getAllIngresosDetailById", error);
-    res.status(500).send({ where: "ERROR AT: /getAllIngresosDetailById", error });
+    res
+      .status(500)
+      .send({ where: "ERROR AT: /getAllIngresosDetailById", error });
   }
 });
 
 router.post("/ingresoAdd", (req, res) => {
   try {
-    const {
-      comentario,
-      id_user_responsable,
-      productos_concat,
-    } = req.body;
+    const { comentario, id_user_responsable, productos_concat } = req.body;
 
     let error = false;
     let mensaje_error = "";
 
-    var listaProductos = productos_concat.split("@");
+    let listaProductos = productos_concat.split("@");
 
     for (let i = 0; i < listaProductos.length && !error; i++) {
-      var producto = listaProductos[i].split("|");
+      let producto = listaProductos[i].split("|");
       //si el stock compra a ingresar es NEGATIVO o CERO
       if (parseInt(producto[1]) <= 0) {
         error = true;
@@ -154,16 +156,16 @@ router.post("/ingresoAdd", (req, res) => {
     mysqlConnection.query(
       query,
       [comentario, id_user_responsable, productos_concat],
-      (err, rows, fields) => {
+      (err, rows, _fields) => {
         if (err) {
           console.error("ERROR AT: /ingresoAdd", err);
           res.status(500).send({ where: "ERROR AT: /ingresoAdd", cause: err });
         } else {
           const [RolState] = rows[0];
-          const { state, response } = RolState;
+          let { state, response } = RolState;
           if (state == 1) {
             const [DataState] = rows[1];
-            const { state, message } = DataState;
+            let { state, message } = DataState;
             if (state == 1) {
               res.json({ state: true, message });
             } else {
@@ -192,7 +194,7 @@ router.post("/getAllRetirosDetailById", (req, res) => {
     mysqlConnection.query(
       "CALL getAllRetirosDetailById(?, ?);",
       [id_retiro, id_user_responsable],
-      (err, rows, fields) => {
+      (err, rows, _fields) => {
         if (err) {
           res
             .status(500)
@@ -207,27 +209,25 @@ router.post("/getAllRetirosDetailById", (req, res) => {
     mysqlConnection.end();
   } catch (error) {
     console.error("ERROR AT: /getAllRetirosDetailById", error);
-    res.status(500).send({ where: "ERROR AT: /getAllRetirosDetailById", error });
+    res
+      .status(500)
+      .send({ where: "ERROR AT: /getAllRetirosDetailById", error });
   }
 });
 
-const { sendEmail, armarListaCompras } = require('../utils/correo');
+const { sendEmail, armarListaCompras } = require("../utils/correo");
 
 router.post("/retiroAdd", (req, res) => {
   try {
-    const {
-      comentario,
-      id_user_responsable,
-      productos_concat,
-    } = req.body;
+    const { comentario, id_user_responsable, productos_concat } = req.body;
 
     let error = false;
     let mensaje_error = "";
 
-    var listaProductos = productos_concat.split("@");
+    let listaProductos = productos_concat.split("@");
 
     for (let i = 0; i < listaProductos.length && !error; i++) {
-      var producto = listaProductos[i].split("|");
+      let producto = listaProductos[i].split("|");
       //si el Stock a retirar es NEGATIVO o CERO
       if (parseInt(producto[1]) <= 0) {
         error = true;
@@ -249,9 +249,10 @@ router.post("/retiroAdd", (req, res) => {
 
     let mysqlConnection = connectMysql("/retiroAdd");
 
-    mysqlConnection.query("CALL retiroAdd(?, ?, ?);",
+    mysqlConnection.query(
+      "CALL retiroAdd(?, ?, ?);",
       [comentario, id_user_responsable, productos_concat],
-      (err, rows, fields) => {
+      (err, rows, _fields) => {
         if (err) {
           console.error("ERROR AT: /retiroAdd", err);
           res.status(500).send({ where: "ERROR AT: /retiroAdd", cause: err });
@@ -259,12 +260,17 @@ router.post("/retiroAdd", (req, res) => {
           const [RolData] = rows[0];
           const { state, response } = RolData;
           if (state == 1) {
-            [DataState] = rows[1];
+            const [DataState] = rows[1];
             const { state, procesado, message } = DataState;
             if (state == 1) {
-              if (procesado != null) {// VALIDAR QUE CUANDO NO HAYA PRODUCTOS BAJO NO ENVIE DATA
+              if (procesado != null) {
+                // VALIDAR QUE CUANDO NO HAYA PRODUCTOS BAJO NO ENVIE DATA
                 let emailToSend = armarListaCompras(procesado);
-                sendEmail('jhosepromero14@gmail.com', 'PRODUCTOS A COMPRAR - STOCK BAJO!!!', emailToSend);
+                sendEmail(
+                  "jhosepromero14@gmail.com",
+                  "PRODUCTOS A COMPRAR - STOCK BAJO!!!",
+                  emailToSend
+                );
               }
               res.json({ state: true, message, procesado });
             } else {
